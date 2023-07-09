c4fb9cadf2883e5ac5406f90607b3f35136c24f0
Andrii Mishkovskyi <mishok13@users.noreply.github.com>
2023-07-09 21:14:01 +0200

    More better

--- keyboards/blank_tehnologii/manibus/keymaps/mishok13/keymap.c
+++ keyboards/blank_tehnologii/manibus/keymaps/mishok13/keymap.c
@@ -7,13 +7,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //   ┌────────┬────────┬────────┬────────┬────────┬────────┐                       ┌────────┬────────┬────────┬────────┬────────┬────────┐
       KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
 //   ├────────┼────────┼────────┼────────┼────────┼────────┤                       ├────────┼────────┼────────┼────────┼────────┼────────┤
-      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL,
+      KC_GRV,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL,
 //   ├────────┼────────┼────────┼────────┼────────┼────────┤                       ├────────┼────────┼────────┼────────┼────────┼────────┤
-      LSFT_T(KC_TAB),KC_A, KC_S, KC_D,    KC_F,    KC_G,                            KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, RSFT_T(KC_QUOT),
+      KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                            KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, RSFT_T(KC_QUOT),
 //   ├────────┼────────┼────────┼────────┼────────┼────────┤                       ├────────┼────────┼────────┼────────┼────────┼────────┤
-      KC_PGUP, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                            KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
+      LSFT_T(KC_TAB),KC_Z,KC_X,  KC_C,    KC_V,    KC_B,                            KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
 //   ├────────┼────────┼────────┼────────┼────────┼────────┼──────────────┐     ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-      KC_PGDN, KC_HOME, KC_END,  MO(1),   KC_LGUI, LCTL_T(KC_DEL), LALT_T(KC_SPC),       LCTL_T(KC_ENT),  LALT_T(KC_BSPC), MO(1),   KC_LBRC, KC_RBRC, KC_GRV,  MEH(KC_NO)
+      MEH(KC_NO),KC_PGUP,KC_PGDN,  MO(1), KC_LGUI, LCTL_T(KC_DEL),LALT_T(KC_SPC),LCTL_T(KC_ENT),LALT_T(KC_BSPC),MO(1),KC_LBRC, KC_RBRC, LCA_T(KC_HOME),  MT(MOD_LCTL | MOD_LGUI, KC_END)
 //   └────────┴────────┴────────┴────────┴────────┴────────┴──────────────┘     └────────┴────────┴────────┴────────┴────────┴────────┴────────┘
   ),
 

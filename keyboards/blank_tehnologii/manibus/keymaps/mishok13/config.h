50fd48a9b930088db6f97e3ed3d35cd7902c922d
Andrii Mishkovskyi <548482+mishok13@users.noreply.github.com>
2024-12-03 12:50:21 +0100

    Trying to convince linter to comply

--- keyboards/blank_tehnologii/manibus/keymaps/mishok13/config.h
+++ keyboards/blank_tehnologii/manibus/keymaps/mishok13/config.h
@@ -16,3 +16,7 @@
 
 #pragma once
 #define EE_HANDS
+
+#define COMBO_TERM 30        // how quickly all combo keys must be pressed in succession to trigger
+#define COMBO_MUST_HOLD_MODS // if a combo triggers a modifier, only trigger when the combo is held
+#define COMBO_HOLD_TERM 175  // how long at least one of the combo keys must be held to trigger
